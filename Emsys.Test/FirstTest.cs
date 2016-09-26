0a5497c5cbdbad3a1f4cae2634ecc5f62fd81931
Camila Serena Zeballos <camila.serena@fing.edu.uy>
2016-09-26 18:47:03 -0300

    Proyecto para pruebas unitarias

---
+++ Emsys.Test/FirstTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Emsys.Test
+{
+    [TestFixture]
+    class FirstTest
+    {
+        [Test]
+        public void PositiveTest()
+        {
+            Assert.AreEqual(7, 7);
+        }
+
+        [Test]
+        public void NegativeTest()
+        {
+            Assert.Fail("Shitty testing class");
+        }
+    }
+}
