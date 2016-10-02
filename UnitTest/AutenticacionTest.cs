986bd8ab95ca33a06d2d64e1f9707e82d89d3b04
Pape Serra <juanpserra@gmail.com>
2016-10-02 01:45:02 -0300

    Se agrega test unitario para autenticacion, falta definir.

---
+++ UnitTest/AutenticacionTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    /// <summary>
+    /// Summary description for AutenticacionTest
+    /// </summary>
+    [TestClass]
+    public class AutenticacionTest
+    {
+        public AutenticacionTest()
+        {
+        }
+
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        /// <summary>
+        /// Agrega un usuario y luego ejecuta iniciar sesion por recurso 
+        /// con los datos del usuario agregado.
+        /// </summary>
+        [TestMethod]
+        public void TestLoguearUsuarioRecurso()
+        {
+
+        }
+    }
+}
