8fe7426fc0833201e1584f49bf6b0ac62c51aa3b
Bruno Amaral <bruno.amaral@fing.edu.uy>
2016-10-23 15:59:00 -0300

    quitado de unused imports

--- DataTypeObjetc/DtoApplicationFile.cs
+++ DataTypeObjetc/DtoApplicationFile.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace DataTypeObject
+﻿namespace DataTypeObject
 {
     public class DtoApplicationFile
     {        
--- DataTypeObjetc/DtoRespuestaExterna.cs
+++ DataTypeObjetc/DtoRespuestaExterna.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-
-namespace DataTypeObject
+﻿namespace DataTypeObject
 {
     public class DtoRespuestaExterna
     {
